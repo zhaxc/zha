c85bfd9e8e782926c7655dc47f651d7c6659bc0e
zhaxc <zhaxc0919@aliyun.com>
2017-04-26 15:49:12 +0800

    no message

---
+++ activemq-spring/src/main/java/com/zha/activemq/Receiver.java
@@ -0,0 +1,63 @@
+package com.zha.activemq;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+/**
+ * 消息消费者
+ * 
+ * @author zhaxc
+ * @date 2016年12月20日 下午3:49:48
+ * @version 3.0
+ */
+public class Receiver {
+	public static void main(String[] args) {
+		// ConnectionFactory ：连接工厂，JMS 用它创建连接
+		ConnectionFactory connectionFactory;
+		// Connection ：JMS 客户端到JMS Provider 的连接
+		Connection connection = null;
+		// Session： 一个发送或接收消息的线程
+		Session session;
+		// Destination ：消息的目的地;消息发送给谁.
+		Destination destination;
+		// 消费者，消息接收者
+		MessageConsumer consumer;
+		connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER,
+				ActiveMQConnection.DEFAULT_PASSWORD, "tcp://192.168.126.19:61616");
+		try {
+			// 构造从工厂得到连接对象
+			connection = connectionFactory.createConnection();
+			// 启动
+			connection.start();
+			// 获取操作连接
+			session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
+			// 获取session注意参数值xingbo.xu-queue是一个服务器的queue，须在在ActiveMq的console配置
+			destination = session.createQueue("FirstQueue");
+			consumer = session.createConsumer(destination);
+			while (true) {
+				// 设置接收者接收消息的时间，为了便于测试，这里谁定为100s
+				TextMessage message = (TextMessage) consumer.receive(100000);
+				if (null != message) {
+					System.out.println("收到消息" + message.getText());
+				} else {
+					break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (null != connection)
+					connection.close();
+			} catch (Throwable ignore) {
+			}
+		}
+	}
+}
