7b50543fa36aed26e3e9d98fc2afa9389b455787
zha <zhaxc0919@aliyun.com>
2018-07-30 17:01:53 +0800

    dd

---
+++ zw-home-parent/zw-mybatis-plus/src/main/java/com/zw/service/impl/ParentServiceImpl.java
@@ -0,0 +1,20 @@
+package com.zw.service.impl;
+
+import com.zw.entity.Parent;
+import com.zw.mapper.ParentMapper;
+import com.zw.service.IParentService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author zha
+ * @since 2017-11-29
+ */
+@Service
+public class ParentServiceImpl extends ServiceImpl<ParentMapper, Parent> implements IParentService {
+	
+}
