6f8a6b0d7b24ed3196d73177f1f97756e612c3d4
D.Yang <koyangslash@gmail.com>
2019-03-28 16:16:28 +0800

    dubbo 集成

---
+++ zha-sboot-parent/sboot-web/src/main/java/com/zw/Application.java
@@ -0,0 +1,18 @@
+package com.zw;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by zha on 2019/3/28.
+ */
+@EnableDubbo
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+}
