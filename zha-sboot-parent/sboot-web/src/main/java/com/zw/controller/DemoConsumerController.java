eefdcce9b644e48720fe0c23aa19bfcbd318b10f
D.Yang <koyangslash@gmail.com>
2019-04-01 11:43:27 +0800

    集成swagger2

--- zha-sboot-parent/sboot-web/src/main/java/com/zw/controller/DemoConsumerController.java
+++ zha-sboot-parent/sboot-web/src/main/java/com/zw/controller/DemoConsumerController.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.zw.system.service.IDemoService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -18,7 +19,7 @@ public class DemoConsumerController {
     @Reference(version = "${demo.service.version}")
     private IDemoService demoService;
 
-    @RequestMapping("/sayHello/{name}")
+    @RequestMapping(value = "/sayHello/{name}",method = RequestMethod.GET)
     public String sayHello(@PathVariable("name") String name) {
         return demoService.sayHello(name);
     }
--- zha-sboot-parent/sboot-web/src/main/java/com/zw/controller/UserController.java
+++ zha-sboot-parent/sboot-web/src/main/java/com/zw/controller/UserController.java
@@ -4,6 +4,9 @@ package com.zw.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.zw.system.entity.User;
 import com.zw.system.service.IUserService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +26,9 @@ public class UserController {
     @Reference
     private IUserService userService;
 
-    @RequestMapping("/getUser/{id}")
+    @ApiOperation(value = "获取用户", notes = "根据id获取用户")
+    @ApiImplicitParam(name = "id", value = "用户id", required = true, dataType = "String")
+    @GetMapping("/getUser/{id}")
     public User getUser(@PathVariable("id") String id) {
         try {
             return userService.selectById(1);
